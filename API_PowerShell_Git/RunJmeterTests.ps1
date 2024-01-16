c8cdb4efff5f2977c548a8c22078333c1eed8d18
ACooperHMCTS <ashley.cooper@hmcts.net>
2024-01-16 11:31:57 +0000

    Adding Powershell scripts

---
+++ API_PowerShell_Git/RunJmeterTests.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    [string]$JMeterBatchPath,
+    [string]$JMXFilePath,
+    [string]$OutputLocation,
+    [int]$Users,
+    [int]$Iterations,
+    [int]$RampTime
+)
+
+# Get the current date and time in a formatted string (e.g., "yyyyMMdd_HHmmss")
+#$currentDateTime = Get-Date -Format "yyyyMMdd_HHmmss"
+
+# Construct the output file path using the current date and time
+#$jtlOutputFilePath = Join-Path $OutputLocation
+
+# Build the JMeter command with parameters
+$jmeterCommand = "& $JMeterBatchPath -n -t `"$JMXFilePath`" -l `"$OutputLocation`" -JUSER_COUNT=$Users -JUSER_ITERATIONS=$Iterations -JUSER_RAMP_TIME=$RampTime"
+
+# Execute the JMeter command
+Invoke-Expression $jmeterCommand
