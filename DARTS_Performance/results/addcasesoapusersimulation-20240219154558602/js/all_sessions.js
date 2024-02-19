b88b13135ead71d16e802247fb299d2cc2ea2cb9
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-02-19 16:40:15 +0000

    Added the SOAP requests for GET / Add Cases

---
+++ DARTS_Performance/results/addcasesoapusersimulation-20240219154558602/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1708357559000,1],[1708357560000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
