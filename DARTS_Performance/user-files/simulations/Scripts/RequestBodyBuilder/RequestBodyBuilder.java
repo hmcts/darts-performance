f4a5c5fe452d1da66e571c91e2dc225fc020c0f8
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-02-19 09:37:04 +0000

    Added the GET Audio Request simulation.

--- DARTS_Performance/user-files/simulations/Scripts/RequestBodyBuilder/RequestBodyBuilder.java
+++ DARTS_Performance/user-files/simulations/Scripts/RequestBodyBuilder/RequestBodyBuilder.java
@@ -8,6 +8,9 @@ public class RequestBodyBuilder {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
     private static final String[] REQUEST_TYPES = {"DOWNLOAD", "PLAYBACK"};
 
+    // Define the percentages for each request type (must sum up to 100)
+    private static final int DOWNLOAD_PERCENTAGE = 70; //% chance
+    private static final int PLAYBACK_PERCENTAGE = 30; //% chance
 
     public static String buildRequestBody(String hearingId, String userId, LocalDateTime startTime, LocalDateTime endTime) {
         String startTimeFormatted = formatTime(startTime);
@@ -15,11 +18,11 @@ public class RequestBodyBuilder {
         String requestType = getRandomRequestType();
 
         return String.format("{\"hearing_id\": \"%s\", " +
-                             "\"requestor\": \"%s\", " +
-                             "\"start_time\": \"%s\", " +
-                             "\"end_time\": \"%s\", " +
-                             "\"request_type\": \"%s\"}",
-                             hearingId, userId, startTimeFormatted, endTimeFormatted, requestType);
+                "\"requestor\": \"%s\", " +
+                "\"start_time\": \"%s\", " +
+                "\"end_time\": \"%s\", " +
+                "\"request_type\": \"%s\"}",
+                hearingId, userId, startTimeFormatted, endTimeFormatted, requestType);
     }
 
     private static String formatTime(LocalDateTime time) {
@@ -27,7 +30,13 @@ public class RequestBodyBuilder {
     }
 
     private static String getRandomRequestType() {
-        int randomIndex = ThreadLocalRandom.current().nextInt(REQUEST_TYPES.length);
-        return REQUEST_TYPES[randomIndex];
+        int totalPercentage = DOWNLOAD_PERCENTAGE + PLAYBACK_PERCENTAGE;
+        int randomNumber = ThreadLocalRandom.current().nextInt(totalPercentage) + 1; // Generate random number between 1 and the total percentage
+
+        if (randomNumber <= DOWNLOAD_PERCENTAGE) {
+            return REQUEST_TYPES[0]; // DOWNLOAD
+        } else {
+            return REQUEST_TYPES[1]; // PLAYBACK
+        }
     }
 }
