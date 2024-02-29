ca4e6ac9c1c7813bd6092996af686d3573261a7f
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-02-29 10:35:11 +0000

    Created the AddDocumentToken Soap Request and fixed a problem around the random event code which wasn't passing correctly.

---
+++ DARTS_Performance/results/adddocumentsoaptokensimulation-20240229100019964/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1709200821000,1],[1709200822000,1],[1709200823000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
