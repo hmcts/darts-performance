351ba453fe6389b86c6cf73461c46526668e4210
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-03-21 11:22:37 +0000

    Added the Audio Token for the Soap request.

---
+++ DARTS_Performance_Gradle/src/gatling/java/simulations/Scripts/DartsSoap/AddAudioTokenSimulation.java
@@ -0,0 +1,38 @@
+package simulations.Scripts.DartsSoap;
+
+import simulations.Scripts.Utilities.AppConfig;
+import simulations.Scripts.Utilities.AppConfig.EnvironmentURL;
+import simulations.Scripts.Scenario.DartsSoap.AddAudioTokenScenario;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import java.util.UUID;
+
+public class AddAudioTokenSimulation extends Simulation {
+
+  FeederBuilder<String> feeder = csv(AppConfig.COURT_HOUSE_AND_COURT_ROOMS_FILE_PATH).random();
+  String boundary = UUID.randomUUID().toString();
+
+  {
+    HttpProtocolBuilder httpProtocol = http
+      .proxy(Proxy(AppConfig.PROXY_HOST, AppConfig.PROXY_PORT).httpsPort(AppConfig.PROXY_PORT))
+      .baseUrl(EnvironmentURL.GATEWAY_BASE_URL.getUrl())
+      .inferHtmlResources()
+      .acceptEncodingHeader("gzip,deflate")
+      .contentTypeHeader("multipart/related; type=\"application/xop+xml\"; start=\"<rootpart@soapui.org>\"; start-info=\"text/xml\"; boundary=" + boundary)
+      .userAgentHeader("Apache-HttpClient/4.5.5 (Java/16.0.2)");
+    final ScenarioBuilder scn = scenario("DARTS - GateWay - Soap - AddAudio:POST")
+        .feed(feeder)    
+        .repeat(1)    
+        .on(exec(AddAudioTokenScenario.addAudioToken().feed(feeder))    
+        );    
+  
+    setUp(
+        scn.injectOpen(constantUsersPerSec(1).during(1)).protocols(httpProtocol));
+    }  
+}
--- DARTS_Performance_Gradle/src/gatling/java/simulations/Scripts/Scenario/DartsSoap/AddAudioUserScenario.java
+++ DARTS_Performance_Gradle/src/gatling/java/simulations/Scripts/Scenario/DartsSoap/AddAudioUserScenario.java
@@ -8,18 +8,18 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 import simulations.Scripts.SOAPRequestBuilder.SOAPRequestBuilder;
 
-public final class AddAudioSoapUserScenario {
+public final class AddAudioUserScenario {
 
     private static final FeederBuilder<String> feeder = csv(AppConfig.COURT_HOUSE_AND_COURT_ROOMS_FILE_PATH).random();
     private static final String randomAudioFile = AppConfig.getRandomAudioFile();
 
-    private AddAudioSoapUserScenario() {}
+    private AddAudioUserScenario() {}
 
-    public static ChainBuilder addAudioSOAPUser() {
+    public static ChainBuilder addAudioUser() {
         return group("AddAudio SOAP Request Group")
             .on(exec(feed(feeder))
             .exec(session -> {
-                String xmlPayload = SOAPRequestBuilder.AddAudioSOAPRequest(session);
+                String xmlPayload = SOAPRequestBuilder.AddAudioUserRequest(session);
                 return session.set("xmlPayload", xmlPayload);
             })
             .exec(http("DARTS - GateWay - Soap - AddAudio - User")
