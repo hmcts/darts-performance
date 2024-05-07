2930e82d99a7eae4e19c8939a185cbb6cf96da5d
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-05-07 09:49:19 +0100

    Made changes to get the Project working. Moved over the Simulations, scenarios, app config and utilities from the other project to this one

---
+++ DARTS_Performance_Gradle/src/gatling/simulations/Scripts/DartsPortal/TranscriberAttachfileAndDownlaodAudioSimulation.java
@@ -0,0 +1,40 @@
+package simulations.Scripts.DartsPortal;
+
+import simulations.Scripts.Utilities.AppConfig;
+import simulations.Scripts.Utilities.Feeders;
+import simulations.Scripts.Scenario.DartsPortal.DartsPortalLoginScenario;
+import simulations.Scripts.Scenario.DartsPortal.DartsPortalLogoutScenario;
+import simulations.Scripts.Scenario.DartsPortal.DartsPortalRequestAudioScenario;
+import simulations.Scripts.Scenario.DartsPortal.TranscriberAttachfileAndDownlaodAudioScenario;
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class TranscriberAttachfileAndDownlaodAudioSimulation extends Simulation {   
+  {
+      HttpProtocolBuilder httpProtocol = http
+        .proxy(Proxy(AppConfig.PROXY_HOST, AppConfig.PROXY_PORT))
+        .baseUrl(AppConfig.EnvironmentURL.B2B_Login.getUrl())
+        .inferHtmlResources()
+        .acceptHeader("application/json, text/plain, */*")
+        .acceptEncodingHeader("gzip, deflate, br")
+        .acceptLanguageHeader("en-US,en;q=0.9")
+        .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");   
+
+    final ScenarioBuilder scn1 = scenario("Darts Portal Login")
+        .exec(feed(Feeders.createTranscriberUsers()))
+        .exec(feed(Feeders.createJudgesFeeder()))
+        .exec(DartsPortalLoginScenario.DartsPortalLoginRequest())
+        .exec(DartsPortalRequestAudioScenario.DartsPortalRequestAudioDownload())
+        .exec(TranscriberAttachfileAndDownlaodAudioScenario.TranscriberAttachfileAndDownlaodAudio())
+        .exec(DartsPortalLogoutScenario.DartsPortalLogoutRequest());
+
+    setUp(
+        scn1.injectOpen(constantUsersPerSec(1).during(1)).protocols(httpProtocol));
+    }    
+} 
+    
+
---
+++ DARTS_Performance_Gradle/src/gatling/simulations/Scripts/Scenario/DartsPortal/DartsPortalLogoutScenario.java
@@ -0,0 +1,40 @@
+package simulations.Scripts.Scenario.DartsPortal;
+
+import simulations.Scripts.Headers.Headers;
+import simulations.Scripts.Utilities.AppConfig;
+import io.gatling.javaapi.core.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public final class DartsPortalLogoutScenario {
+
+
+    private DartsPortalLogoutScenario() {}
+
+    public static ChainBuilder DartsPortalLogoutRequest() {
+        return group("Darts Portal Logout")
+            .on(
+              exec(
+                http("Darts-Portal - Auth - Logout")
+                .get(AppConfig.EnvironmentURL.DARTS_PORTAL_BASE_URL.getUrl() + "/auth/logout")
+                .headers(Headers.portalLoginHeaders(Headers.PortalCommonHeaders))
+                )  
+              .exec(
+                    http("Darts-Portal - Auth - Logout-callback")
+                    .get(AppConfig.EnvironmentURL.DARTS_PORTAL_BASE_URL.getUrl() + "/auth/logout-callback")
+                    .headers(Headers.portalLoginHeaders(Headers.PortalCommonHeaders))                   
+                  )
+              .exec(session -> {
+                  Object stateProperties = session.get("stateProperties");
+                  System.out.println("Extracted StateProperties: " + stateProperties);
+                  return session;
+              })
+              .exec(
+                http("Darts-Portal - App - Config")
+                  .get(AppConfig.EnvironmentURL.DARTS_PORTAL_BASE_URL.getUrl() + "/app/config")
+                  .headers(Headers.PerftraceHeaders(Headers.PortalCommonHeaders))
+              )
+            );
+      }  
+}
