eb0d13423ca0dc6cc7d252088546e4c498f466e1
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-05-16 14:32:45 +0100

    Added gatling tests

---
+++ src/gatling/java/simulations/Scripts/Scenario/DartsSoap/GetCourtlogTokenScenario.java
@@ -0,0 +1,28 @@
+package simulations.Scripts.Scenario.DartsSoap;
+
+import simulations.Scripts.Headers.Headers;
+import simulations.Scripts.Utilities.AppConfig.SoapServiceEndpoint;
+import io.gatling.javaapi.core.*;
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+import simulations.Scripts.SOAPRequestBuilder.SOAPRequestBuilder;
+
+public final class GetCourtlogTokenScenario {
+
+    private GetCourtlogTokenScenario() {}
+    public static ChainBuilder getCourtLogToken() {
+        return group("CourtLog SOAP Request Group")
+            .on(exec(session -> {
+                    String xmlPayload = SOAPRequestBuilder.GetCourtLogTokenRequest(session);
+                    return session.set("xmlPayload", xmlPayload);
+                })
+                .exec(http("DARTS - GateWay - Soap - Get CourtLog - Token")
+                        .post(SoapServiceEndpoint.StandardService.getEndpoint())
+                        .headers(Headers.SoapHeaders)
+                        .body(StringBody(session -> session.get("xmlPayload")))
+                        .check(status().is(200))
+                        .check(xpath("//return/code").saveAs("statusCode"))
+                        .check(xpath("//return/message").saveAs("message"))
+            ));
+    } 
+}
