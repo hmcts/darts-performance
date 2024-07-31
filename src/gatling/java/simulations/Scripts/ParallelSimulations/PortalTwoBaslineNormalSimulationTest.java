7ea41e58cfa6cdcb60dfe08181f01ca966dd7b00
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-07-31 10:39:20 +0100

    Parameter changes and added the portal simulations

---
+++ src/gatling/java/simulations/Scripts/ParallelSimulations/PortalTwoBaslineNormalSimulationTest.java
@@ -0,0 +1,132 @@
+package simulations.Scripts.ParallelSimulations;
+
+import simulations.Scripts.Utilities.AppConfig;
+import simulations.Scripts.Utilities.Feeders;
+import simulations.Scripts.Scenario.DartsPortal.DartsPortalInternalLoginScenario;
+import simulations.Scripts.Scenario.DartsPortal.DartsPortalApproveAudioScenario;
+import simulations.Scripts.Scenario.DartsPortal.DartsPortalExternalLoginScenario;
+import simulations.Scripts.Scenario.DartsPortal.DartsPortalInternalLogoutScenario;
+import simulations.Scripts.Scenario.DartsPortal.DartsPortalRequestAudioScenario;
+import simulations.Scripts.Scenario.DartsPortal.DartsPortalRequestTranscriptionScenario;
+import simulations.Scripts.Scenario.DartsPortal.TranscriberAttachFileAndDownloadAudioScenario;
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+import java.time.Duration;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class PortalTwoBaslineNormalSimulationTest extends Simulation {   
+
+    private static final String BASELINE_NORMAL_JUDGE_USERS = "Baseline Normal - DARTS - Portal - Judge Users";
+    private static final String BASELINE_NORMAL_COURT_CLERK_USERS = "Baseline Normal - DARTS - Portal - Court Clerk Users";
+    private static final String BASELINE_NORMAL_COURT_MANAGER_USERS = "Baseline Normal - DARTS - Portal - Court Manager Users ";
+    private static final String BASELINE_NORMAL_TRANSCRIBER_USERS = "Baseline Normal - DARTS - Portal - Transcriber Users ";
+    private static final String BASELINE_NORMAL_LANGUAGE_USERS = "Baseline Normal - DARTS - Portal - Language Shop Users";
+
+    @Override
+    public void before() {
+        System.out.println("Simulation is about to start!");
+    }
+
+    public PortalTwoBaslineNormalSimulationTest() {
+            HttpProtocolBuilder httpProtocolExternal = http
+                .proxy(Proxy(AppConfig.PROXY_HOST, AppConfig.PROXY_PORT))
+                .baseUrl(AppConfig.EnvironmentURL.B2B_Login.getUrl())
+                .inferHtmlResources()
+                .acceptHeader("application/json, text/plain, */*")
+                .acceptEncodingHeader("gzip, deflate, br")
+                .acceptLanguageHeader("en-US,en;q=0.9")
+                .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");   
+
+    
+            HttpProtocolBuilder httpProtocolInternal = http
+            .proxy(Proxy(AppConfig.PROXY_HOST, AppConfig.PROXY_PORT))
+                //.baseUrl(AppConfig.EnvironmentURL.B2B_Login.getUrl())
+                .baseUrl("https://login.microsoftonline.com") 
+                .inferHtmlResources()
+                .acceptHeader("application/json, text/plain, */*")
+                .acceptEncodingHeader("gzip, deflate, br")
+                .acceptLanguageHeader("en-US,en;q=0.9")
+                .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
+              
+    
+                setUpScenarios(httpProtocolExternal, httpProtocolInternal);
+            }
+           
+        
+    private void setUpScenarios(HttpProtocolBuilder httpProtocolExternal, HttpProtocolBuilder httpProtocolInternal) {
+        // Set up scenarios with configurable parameters
+        ScenarioBuilder smokeJudgeUsers = setUpJudgeUsers(BASELINE_NORMAL_JUDGE_USERS);
+        ScenarioBuilder smokeCourtClerkUsers = setUpCourtClerkUsers(BASELINE_NORMAL_COURT_CLERK_USERS);
+        ScenarioBuilder smokeCourtManagerUsers = setUpCourtManagerUsers(BASELINE_NORMAL_COURT_MANAGER_USERS);
+        ScenarioBuilder smokeTranscriberUsers = setUpTranscriberUsers(BASELINE_NORMAL_TRANSCRIBER_USERS);
+        ScenarioBuilder smokeLanguageShopUsers = setUpLanguageShopUsers(BASELINE_NORMAL_LANGUAGE_USERS);
+
+        // Call setUp once with all scenarios
+        setUp(
+            smokeJudgeUsers.injectOpen(
+                rampUsers(AppConfig.JUDGE_RAMP_UP_USERS).during(Duration.ofMinutes(AppConfig.RAMP_UP_DURATION_OF_JUDGES)) 
+            ).protocols(httpProtocolInternal),
+            smokeCourtClerkUsers.injectOpen(
+                rampUsers(AppConfig.COURT_CLERK_RAMP_UP_USERS).during(Duration.ofMinutes(AppConfig.RAMP_UP_DURATION_OF_COURT_CLERK)) 
+            ).protocols(httpProtocolInternal),
+            smokeCourtManagerUsers.injectOpen(
+                rampUsers(AppConfig.COURT_MANAGER_RAMP_UP_USERS).during(Duration.ofMinutes(AppConfig.RAMP_UP_DURATION_OF_COURT_MANAGER)) 
+            ).protocols(httpProtocolInternal),
+            smokeTranscriberUsers.injectOpen(
+                rampUsers(AppConfig.TRANSCRIBER_RAMP_UP_USERS).during(Duration.ofMinutes(AppConfig.RAMP_UP_DURATION_OF_TRANSCRIBER))
+            ).protocols(httpProtocolExternal),
+            smokeLanguageShopUsers.injectOpen(
+                rampUsers(AppConfig.LANGUAGE_SHOP_RAMP_UP_USERS).during(Duration.ofMinutes(AppConfig.RAMP_UP_DURATION_OF_LANGUAGE_SHOP)) 
+            ).protocols(httpProtocolExternal)
+        );
+    }
+
+    private ScenarioBuilder setUpTranscriberUsers(String scenarioName) {
+        return scenario(scenarioName)        
+            .exec(feed(Feeders.createTranscriberUsers()))
+            .exec(DartsPortalExternalLoginScenario.DartsPortalExternalLoginRequest())
+            .repeat(1)
+            .on(exec(DartsPortalRequestAudioScenario.DartsPortalRequestAudioDownload()))
+            .exec(TranscriberAttachFileAndDownloadAudioScenario.TranscriberAttachfileAndDownlaodAudio())
+            .exec(DartsPortalExternalLoginScenario.DartsPortalExternalLoginRequest());
+    }
+
+    private ScenarioBuilder setUpCourtClerkUsers(String scenarioName) {
+        return scenario(scenarioName)        
+            .exec(feed(Feeders.createCourtClerkUsers()))
+            .exec(DartsPortalInternalLoginScenario.DartsPortalInternalLoginRequest())      
+            .exec(DartsPortalRequestAudioScenario.DartsPortalRequestAudioDownload())
+            .exec(DartsPortalRequestTranscriptionScenario.DartsPortalRequestTranscription())
+            .exec(DartsPortalInternalLogoutScenario.DartsPortalInternalLogoutRequest());
+    }
+
+    private ScenarioBuilder setUpCourtManagerUsers(String scenarioName) {
+        return scenario(scenarioName)        
+            .exec(feed(Feeders.createCourtManagerUsers()))
+            .exec(DartsPortalInternalLoginScenario.DartsPortalInternalLoginRequest())      
+            .repeat(20).on(
+            exec(DartsPortalApproveAudioScenario.DartsPortalApproveAudio()))
+            .exec(DartsPortalInternalLogoutScenario.DartsPortalInternalLogoutRequest());
+    }
+
+    private ScenarioBuilder setUpLanguageShopUsers(String scenarioName) {
+        return scenario(scenarioName)        
+            .exec(feed(Feeders.createLanguageShopUsers()))
+            .exec(DartsPortalExternalLoginScenario.DartsPortalExternalLoginRequest())
+            .exec(DartsPortalRequestAudioScenario.DartsPortalRequestAudioDownload())
+            .exec(DartsPortalInternalLogoutScenario.DartsPortalInternalLogoutRequest());
+    }
+
+    private ScenarioBuilder setUpJudgeUsers(String scenarioName) {
+        return scenario(scenarioName)        
+            .exec(feed(Feeders.createJudgeUsers()))
+            .exec(DartsPortalInternalLoginScenario.DartsPortalInternalLoginRequest())
+            .exec(DartsPortalInternalLogoutScenario.DartsPortalInternalLogoutRequest());
+    }
+    @Override
+    public void after() {
+        System.out.println("Simulation is finished!");
+    }
+}
