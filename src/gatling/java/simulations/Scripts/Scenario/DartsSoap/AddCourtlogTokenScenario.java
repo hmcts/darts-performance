0eff98565812af424185912d8f1e2480d911c34f
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-08-08 17:21:04 +0100

    Fixed the rest of the broken test Soap tests. All now working

--- src/gatling/java/simulations/Scripts/Scenario/DartsSoap/AddCourtlogTokenScenario.java
+++ src/gatling/java/simulations/Scripts/Scenario/DartsSoap/AddCourtlogTokenScenario.java
@@ -2,6 +2,7 @@ package simulations.Scripts.Scenario.DartsSoap;
 
 import simulations.Scripts.Headers.Headers;
 import simulations.Scripts.Utilities.AppConfig.SoapServiceEndpoint;
+import simulations.Scripts.Utilities.Feeders;
 import io.gatling.javaapi.core.*;
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
@@ -12,7 +13,8 @@ public final class AddCourtlogTokenScenario {
     private AddCourtlogTokenScenario() {}
     public static ChainBuilder addCourtLogToken() {
         return group("CourtLog SOAP Request Group")
-            .on(exec(session -> {
+            .on(feed(Feeders.createCourtHouseAndCourtRooms()) 
+            .exec(session -> {
                     String xmlPayload = SOAPRequestBuilder.AddCourtLogTokenRequest(session);
                     return session.set("xmlPayload", xmlPayload);
                 })
