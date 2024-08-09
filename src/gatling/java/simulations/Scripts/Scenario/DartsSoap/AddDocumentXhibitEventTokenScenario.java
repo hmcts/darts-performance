91a160cf702306f20b879431a568c9af321009dd
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-08-09 09:08:29 +0100

    Made some logging improvements to try and state when 200 response happens but there is a error within the message

--- src/gatling/java/simulations/Scripts/Scenario/DartsSoap/AddDocumentXhibitEventTokenScenario.java
+++ src/gatling/java/simulations/Scripts/Scenario/DartsSoap/AddDocumentXhibitEventTokenScenario.java
@@ -26,16 +26,26 @@ public final class AddDocumentXhibitEventTokenScenario {
                         .check(xpath("//messageId/text()").find().optional().saveAs("messageId"))
                         .check(xpath("//return/code").saveAs("statusCode"))
                         .check(xpath("//return/message").saveAs("message"))
-                )
-                .exec(session -> {
-                    Object messageId = session.get("messageId");
-                    if (messageId != null) {
-                        System.out.println("messageId: " + messageId.toString());
-                    } else {
-                        System.out.println("No value for messageId.");
-                    }
-                    return session;
-                })
-            );
-    } 
-}
+                        )
+                        .exec(session -> {
+                            String statusCode = session.getString("statusCode");
+                            String message = session.getString("message");
+                            if (statusCode.equals("ERROR") || (message != null && message.toLowerCase().contains("error"))) {
+                                // Mark the request as failed if there's an error message
+                                session.markAsFailed();
+                                System.out.println("Error detected for Xhibit Event: " + message);
+                            }
+                            return session;
+                        })
+                        .exec(session -> {
+                            Object messageId = session.get("messageId");
+                            if (messageId != null) {
+                                System.out.println("messageId: " + messageId.toString());
+                            } else {
+                                System.out.println("No value for messageId.");
+                            }
+                            return session;
+                        })
+                    );
+            } 
+        }
