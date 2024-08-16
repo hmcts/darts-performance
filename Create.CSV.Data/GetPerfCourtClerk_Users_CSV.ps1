f50330d7fcf33b8f0c021e0dc8efe19f028605d4
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-08-16 14:55:01 +0100

    Fixed a script issue around closing events for Retentions

--- Create.CSV.Data/GetPerfCourtClerk_Users_CSV.ps1
+++ Create.CSV.Data/GetPerfCourtClerk_Users_CSV.ps1
@@ -136,7 +136,7 @@ if (Test-Path -Path $outputFile) {
 }
 
 # Export column headers to a new CSV file
-$headers = "Email,Password,user_name,cth_id,courthouse_name,courthouse_code,Type,defendant_name"
+$headers = "Email,Password,user_name,cth_id,courthouse_name,courthouse_code,Type,defendantFirstName,defendantSecondName,defendantThirdName,defendantFourthName,defendantFifthName"
 $headers | Out-File -FilePath $outputFile -Encoding ASCII
 
 # Append the query results to the CSV file with comma delimiters
