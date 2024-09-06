c8f2a2861c42417f9ba5ad43c3c197068adf2a48
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-09-06 15:19:01 +0100

    Changes to the judges search

--- src/gatling/java/simulations/Scripts/Scenario/DartsPortal/DartsPortalChangeRetentionScenario.java
+++ src/gatling/java/simulations/Scripts/Scenario/DartsPortal/DartsPortalChangeRetentionScenario.java
@@ -35,23 +35,58 @@ public final class DartsPortalChangeRetentionScenario {
           // Initialize `caseCount` to 0 before starting the search
           .exec(session -> session.set("caseCount", 0))
 
-          .exec(http("Darts-Portal - Api - Cases - Search")
-              .post(AppConfig.EnvironmentURL.DARTS_PORTAL_BASE_URL.getUrl() + "/api/cases/search")
-              .headers(Headers.searchCaseHeaders(Headers.CommonHeaders))              
-              .body(StringBody(session -> session.get("xmlPayload"))).asJson()
-              .check(status().is(200))
-              .check(jsonPath("$.title").optional().saveAs("errorTitle"))              
+          .exec(session -> {
+            String searchRequestPayload = RequestBodyBuilder.buildSearchCaseRequestBody(session);
+            return session.set("searchRequestPayload", searchRequestPayload);
+          })
+          .asLongAs(session -> !session.contains("caseCount") || session.getInt("caseCount") == 0)
+          .on(
+              exec(http("Darts-Portal - Api - Cases - Search")
+                  .post(AppConfig.EnvironmentURL.DARTS_PORTAL_BASE_URL.getUrl() + "/api/cases/search")
+                  .headers(Headers.searchCaseHeaders(Headers.CommonHeaders))
+                  .body(StringBody(session -> session.get("searchRequestPayload"))).asJson()
+                  // Check if the status is 200 to consider it as passed
+                  .check(status().is(200))
+                  .check(status().saveAs("status"))
+                  .check(jsonPath("$[*].case_id").count().saveAs("caseCount"))
+                  .check(jsonPath("$[*].case_id").findRandom().optional().saveAs("getCaseId"))
               )
-              .exec(session -> {                
-                Object errorTitle = session.get("errorTitle");
-                if (errorTitle != null) {
-                    String errorMessage = "Request failed with error: " + errorTitle.toString();
-                    System.out.println(errorMessage);
-                    throw new RuntimeException(errorMessage); // Fail the test by throwing an exception
-                }
-                return session;
-            }
+              .exec(session -> {
+                  int caseCount = session.getInt("caseCount");
+                  if (caseCount == 0) {
+                      // Handle empty response
+                      System.out.println("Empty response received. Marking as passed and retrying...");
+                      String searchPayload = RequestBodyBuilder.buildSearchCaseRequestBody(session);
+                      return session.set("searchRequestPayload", searchPayload).markAsSucceeded();
+                  } else {
+                      return session;
+                  }
+              })
+              .exec(session -> {
+                  int statusCode = session.getInt("status");
+                  if (statusCode == 400 || statusCode == 502 || statusCode == 504) {
+                      return session.markAsFailed();  // Mark as failed to trigger logging in UserInfoLogger
+                  }
+                  return session;
+              })
           )
+          .exec(UserInfoLogger.logDetailedErrorMessage("Darts-Portal - Api - Cases - Search"))
+
+          .exec(session -> {
+              // Log non-empty response
+              System.out.println("Non-empty response received.");
+              return session;
+          })
+          .exec(session -> {
+              Object getCaseId = session.get("getCaseId");
+              if (getCaseId != null) {
+                  System.out.println("getCaseId: " + getCaseId.toString());
+              } else {
+                  System.out.println("No value saved using saveAs.");
+              }
+              return session;
+          }
+          )          
           .exitHereIfFailed()
           .pause(3)
           .exec(
@@ -59,7 +94,6 @@ public final class DartsPortalChangeRetentionScenario {
               .get(AppConfig.EnvironmentURL.DARTS_PORTAL_BASE_URL.getUrl() + "/auth/is-authenticated?t=" + randomNumber.nextInt())
               .headers(Headers.CommonHeaders)
           )
-          .exec(UserInfoLogger.logDetailedErrorMessage("Darts-Portal - Auth - Is-authenticated"))
 
           .exec(
             http("Darts-Portal - Api - Cases")
@@ -94,7 +128,6 @@ public final class DartsPortalChangeRetentionScenario {
               .get(AppConfig.EnvironmentURL.DARTS_PORTAL_BASE_URL.getUrl() + "/auth/is-authenticated?t=" + randomNumber.nextInt())
               .headers(Headers.CommonHeaders)
           )
-          .exec(UserInfoLogger.logDetailedErrorMessage("Darts-Portal - Auth - Is-authenticated"))
 
           .exec(
             http("Darts-Portal - Api - Cases")
@@ -108,8 +141,6 @@ public final class DartsPortalChangeRetentionScenario {
               .get(AppConfig.EnvironmentURL.DARTS_PORTAL_BASE_URL.getUrl() + "/auth/is-authenticated?t=" + randomNumber.nextInt())
               .headers(Headers.CommonHeaders)
               )
-          .exec(UserInfoLogger.logDetailedErrorMessage("Darts-Portal - Auth - Is-authenticated"))
-
           .exec(
             http("Darts-Portal - User - Refresh-profile")
             .post(AppConfig.EnvironmentURL.DARTS_PORTAL_BASE_URL.getUrl() + "/user/refresh-profile")
