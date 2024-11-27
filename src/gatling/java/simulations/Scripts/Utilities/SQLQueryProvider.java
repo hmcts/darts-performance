8ab5b379d97a69ff45f9399e760ec2268c6d76ed
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-11-27 09:26:20 +0000

    Removed the changes I did to the SQL query to get data.

--- src/gatling/java/simulations/Scripts/Utilities/SQLQueryProvider.java
+++ src/gatling/java/simulations/Scripts/Utilities/SQLQueryProvider.java
@@ -1,60 +1,110 @@
 package simulations.Scripts.Utilities;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class SQLQueryProvider {
 
-    private static final String SQL_DIRECTORY = "src/main/resources/sql/";
-
-    // Method to read the SQL file
-    public static String loadSQL(String fileName) {
-        try {
-            return new String(Files.readAllBytes(Paths.get(SQL_DIRECTORY + fileName)));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load SQL file: " + fileName, e);
-        }
-    }
-    
     public static String getHearingQuery() {
-        return loadSQL("hearing_query.sql");
-    }
-
-    public static String getTransformedMediaToDeleteQuery() {
-        return loadSQL("transformed_media_delete_query.sql");
-    }
-
-    public static String getCaseDetailsForRetentionQuery() {
-        return loadSQL("case_details_for_retention_query.sql");
+        return "SELECT " +
+            "   subquery.hea_id, " +
+            "   subquery.start_ts, " +
+            "   subquery.end_ts, " +
+            "   subquery.usr_id " +
+            "FROM ( " +
+            "   SELECT " +
+            "       h.hea_id, " +
+            "       TO_CHAR(m.start_ts AT TIME ZONE 'UTC', 'YYYY-MM-DD\"T\"HH24:MI:SS.MS\"z\"') AS start_ts, " +
+            "       TO_CHAR(m.end_ts AT TIME ZONE 'UTC', 'YYYY-MM-DD\"T\"HH24:MI:SS.MS\"z\"') AS end_ts, " +
+            "       sguae.usr_id " +
+            "   FROM " +
+            "       darts.hearing h " +
+            "   INNER JOIN " +
+            "       darts.courtroom cr ON h.ctr_id = cr.ctr_id " +
+            "   INNER JOIN " +
+            "       darts.courthouse ch ON cr.cth_id = ch.cth_id " +
+            "   INNER JOIN " +
+            "       darts.court_case cc ON cc.cas_id = h.cas_id " +
+            "   INNER JOIN " +
+            "       darts.hearing_media_ae hma ON hma.hea_id = h.hea_id " +
+            "   INNER JOIN " +
+            "       darts.media m ON m.med_id = hma.med_id " +
+            "   INNER JOIN " +
+            "       darts.security_group_courthouse_ae sgcae ON ch.cth_id = sgcae.cth_id " +
+            "   INNER JOIN " +
+            "       darts.security_group_user_account_ae sguae ON sgcae.grp_id = sguae.grp_id " +
+            "   WHERE " +
+            "       h.hearing_date BETWEEN '2023-02-26' AND '2024-05-27' " +
+            "   AND " +
+            "       sguae.usr_id NOT IN (-100, -99, -69, -68, -67, -48, -44, -4, -3, -2, -1, 0, 1, -101, 221, 241, 1141) " +
+            "   ORDER BY " +
+            "       RANDOM() " +
+            "   LIMIT 1000 " +
+            ") subquery;";
     }
 
     public static String getTransformedMediaIdForDownloadQuery() {
-        return loadSQL("transformed_media_download_query.sql");
+        return "SELECT darts.transformed_media.trm_id, " + 
+            "darts.transformed_media.mer_id, " + 
+            "darts.media_request.hea_id, " + 
+            "darts.media_request.request_status, " + 
+            "darts.media_request.request_type " + 
+            "FROM darts.transformed_media " + 
+            "INNER JOIN " + 
+            "    darts.media_request " +  
+            "    ON " + 
+            "    darts.transformed_media.mer_id = darts.media_request.mer_id " + 
+            "WHERE darts.media_request.request_type = 'DOWNLOAD' " + 
+            "AND darts.media_request.request_status = 'COMPLETED' " + 
+            "ORDER BY trm_id DESC LIMIT 1000;"; 
     }
 
     public static String getTransformedMediaIdForPlayBackQuery() {
-        return loadSQL("transformed_media_playback_query.sql");
+        return "SELECT darts.transformed_media.trm_id, " + 
+                "darts.transformed_media.mer_id, " + 
+                "darts.media_request.hea_id, " + 
+                "darts.media_request.request_status, " + 
+                "darts.media_request.request_type " + 
+                "FROM darts.transformed_media " + 
+                "INNER JOIN " + 
+                "    darts.media_request " +  
+                "    ON " + 
+                "    darts.transformed_media.mer_id = darts.media_request.mer_id " + 
+                "WHERE darts.media_request.request_type = 'PLAYBACK' " + 
+                "AND darts.media_request.request_status = 'COMPLETED' " + 
+                "ORDER BY trm_id DESC LIMIT 1000;"; 
     }
 
-    public static String getLanguageShopUsersQuery() {
-        return loadSQL("language_shop_users_query.sql");
-    }
+    public static String getCaseDetailsForRetentionQuery() {
+        return "WITH random_cases AS (" +
+            "    SELECT DISTINCT ON (cth_id) cas_id, cth_id " +
+            "    FROM darts.court_case " +
+            "    ORDER BY cth_id, RANDOM()" +
+            "), " +
+            "case_with_courtrooms AS (" +
+            "    SELECT rc.cas_id, cc.case_number, rc.cth_id, ch.courthouse_name, " +
+            "           (SELECT cr.courtroom_name " +
+            "            FROM darts.courtroom cr " +
+            "            WHERE cr.cth_id = rc.cth_id " +
+            "            ORDER BY RANDOM() LIMIT 1) AS courtroom_name " +
+            "    FROM random_cases rc " +
+            "    JOIN darts.court_case cc ON rc.cas_id = cc.cas_id " +
+            "    JOIN darts.courthouse ch ON rc.cth_id = ch.cth_id " +
+            ") " +
+            "SELECT cas_id, case_number, cth_id, courthouse_name, courtroom_name " +
+            "FROM case_with_courtrooms " +
+            "ORDER BY cth_id;";
+        }
 
-    public static String getCourtManagerUsersQuery() {
-        return loadSQL("court_manager_users_query.sql");
-    }
-
-    public static String getCourtClerkUsersQuery() {
-        return loadSQL("court_clerk_users_query.sql");
-    }
-
-    public static String getTranscriberUsersQuery() {
-        return loadSQL("transcriber_users_query.sql");
-    }
-
-    public static String getJudgeUsersQuery() {
-        return loadSQL("judge_users_query.sql");
-    }
-
+    public static String getTransformedMediaToDeleteQuery() {
+        return  "SELECT darts.transformed_media.trm_id, " +
+                "darts.transformed_media.mer_id, " +
+                "darts.media_request.hea_id, " +
+                "darts.media_request.request_status, " +
+                "darts.media_request.request_type " +
+            "FROM darts.transformed_media " +
+            "INNER JOIN " +
+                "darts.media_request " +
+            "ON " +
+                "darts.transformed_media.mer_id = darts.media_request.mer_id " +
+            "WHERE darts.media_request.request_type = 'DOWNLOAD' " +
+            "AND darts.media_request.request_status != 'DELETED' " +
+            "ORDER BY trm_id ASC LIMIT 500;"; 
+        }       
 }
