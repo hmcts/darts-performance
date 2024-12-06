896376db3fd615c8f9d590ed2c4ed5fc11fa3631
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2024-12-06 11:00:32 +0000

    Made some changes to enable to simulation to run that I missed before

---
+++ RunScripts/RunDartsSoapTest.ps1
@@ -0,0 +1,8 @@
+﻿# Define the path to the target directory
+$targetPath = "C:\darts-performance"
+
+# Navigate to the target directory
+Set-Location -Path $targetPath
+
+# Execute the Gradle command
+& .\gradlew.bat runSoapTestSimulation -DtestType=smoke
