5ab44f5b19e8f1cbe987e3551c7634d447bf4264
ACooperHMCTS <149593346+ACooperHMCTS@users.noreply.github.com>
2025-05-01 16:11:42 +0100

    Created the E2E Simulation.

---
+++ src/gatling/java/simulations/Scripts/ScenarioBuilder/AutomatedTaskScenario.java
@@ -0,0 +1,28 @@
+package simulations.Scripts.ScenarioBuilder;
+
+import simulations.Scripts.Scenario.DartsApi.GetApiTokenScenario;
+import simulations.Scripts.Scenario.DartsApi.RunApplyRetentionCaseAssociatedObjectsTaskScenario;
+import simulations.Scripts.Scenario.DartsApi.RunApplyRetentionTaskScenario;
+import simulations.Scripts.Scenario.DartsApi.RunInboundToUnstructuredDataStoreScenario;
+import simulations.Scripts.Scenario.DartsApi.RunProcessArmResponseFilesTaskScenario;
+import simulations.Scripts.Scenario.DartsApi.RunUnstructuredToArmDataStoreScenario;
+import io.gatling.javaapi.core.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+
+public class AutomatedTaskScenario {
+   public static ScenarioBuilder build(String scenarioName) {
+    return scenario(scenarioName)
+        .group("Automated Tasks")
+        .on(exec(GetApiTokenScenario.getApiToken())
+                .repeat(1)    
+                .on(exec(RunInboundToUnstructuredDataStoreScenario.RunInboundToUnstructuredDataStore())
+                .exec(RunApplyRetentionTaskScenario.RunApplyRetentionTask())
+                .exec(RunApplyRetentionCaseAssociatedObjectsTaskScenario.RunApplyRetentionCaseAssociatedObjectsTask())
+                .exec(RunUnstructuredToArmDataStoreScenario.RunUnstructuredToArmDataStore())
+                .exec(RunProcessArmResponseFilesTaskScenario.RunProcessArmResponseFilesTask())
+            )
+        );
+    }
+}
+        
