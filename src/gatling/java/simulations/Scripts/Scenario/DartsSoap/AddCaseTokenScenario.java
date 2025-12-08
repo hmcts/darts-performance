ff7f0255ab3e679b6cdd18a75bc15940cef937db
benedwards <ben.edwards@cgi.com>
2025-12-08 19:55:42 +0000

    Added missing log

--- src/gatling/java/simulations/Scripts/Scenario/DartsSoap/AddCaseTokenScenario.java
+++ src/gatling/java/simulations/Scripts/Scenario/DartsSoap/AddCaseTokenScenario.java
@@ -1,5 +1,6 @@
 package simulations.Scripts.Scenario.DartsSoap;
 
+import lombok.extern.slf4j.Slf4j;
 import simulations.Scripts.Utilities.*;
 import simulations.Scripts.Utilities.AppConfig.SoapServiceEndpoint;
 import io.gatling.javaapi.core.*;
@@ -7,6 +8,7 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 import simulations.Scripts.SOAPRequestBuilder.SOAPRequestBuilder;
 
+@Slf4j
 public final class AddCaseTokenScenario {
 
     private AddCaseTokenScenario() {}
